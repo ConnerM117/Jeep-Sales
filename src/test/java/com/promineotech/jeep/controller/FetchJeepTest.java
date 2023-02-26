6ceab7ec0d8a11a6e586bd13bfc4b4dae6f071c9
Conner <awesomccall@gmail.com>
2023-02-25 22:17:48 -0700

    Week 14 first commit

--- src/test/java/com/promineotech/jeep/controller/FetchJeepTest.java
+++ src/test/java/com/promineotech/jeep/controller/FetchJeepTest.java
@@ -2,6 +2,8 @@ package com.promineotech.jeep.controller;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import java.math.BigDecimal;
+import java.util.LinkedList;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +18,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
+import com.promineotech.jeep.controller.support.FetchJeepTestSupport;
 import com.promineotech.jeep.entity.Jeep;
 import com.promineotech.jeep.entity.JeepModel;
 import lombok.Getter;
@@ -23,11 +26,11 @@ import lombok.Getter;
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
 @Sql(scripts = {
-    "classpath:flyway/migrations/V1.0_Jeep_Schema.sql",
-    "classpath:flyway/migrations/V1.1_Jeep_Data.sql"},
+    "classpath:flyway/migrations/V1.0__Jeep_Schema.sql",
+    "classpath:flyway/migrations/V1.1__Jeep_Data.sql"},
     config = @SqlConfig(encoding = "utf-8"))
 
-class FetchJeepTest {
+class FetchJeepTest extends FetchJeepTestSupport {
 
   @LocalServerPort
   private int serverPort;
@@ -48,6 +51,29 @@ class FetchJeepTest {
     
     //Then: a success (OK-200) status code is returned
     assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    
+    List<Jeep> expected = buildExpected();
+    assertThat(response.getBody()).isEqualTo(expected);
+  }
+  
+  private List<Jeep> buildExpected() {
+    List<Jeep> list = new LinkedList<>();
+    list.add(Jeep.builder()
+        .modelId(JeepModel.WRANGLER)
+        .trimLevel("Sport")
+        .numDoors(2)
+        .wheelSize(17)
+        .basePrice(new BigDecimal("28475.00"))
+        .build());
+    list.add(Jeep.builder()
+        .modelId(JeepModel.WRANGLER)
+        .trimLevel("Sport")
+        .numDoors(4)
+        .wheelSize(17)
+        .basePrice(new BigDecimal("31975.00"))
+        .build());
+    
+    return list;
   }
 
   protected String getBaseUri() {
